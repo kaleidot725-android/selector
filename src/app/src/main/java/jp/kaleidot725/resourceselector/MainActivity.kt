c745baa9adf2e1d106ecf70982e0647caf3e4bb0
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-03-24 23:23:46 +0900

    Update Sample

--- src/app/src/main/java/jp/kaleidot725/resourceselector/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/resourceselector/MainActivity.kt
@@ -2,11 +2,37 @@ package jp.kaleidot725.resourceselector
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        ok_button.setOnClickListener {
+            Toast.makeText(applicationContext, "OK", Toast.LENGTH_SHORT).show()
+        }
+
+        ng_button.setOnClickListener {
+            Toast.makeText(applicationContext, "NG", Toast.LENGTH_SHORT).show()
+        }
+
+        ok_image_button.setOnClickListener {
+            Toast.makeText(applicationContext, "OK", Toast.LENGTH_SHORT).show()
+        }
+
+        ng_image_button.setOnClickListener {
+            Toast.makeText(applicationContext, "NG", Toast.LENGTH_SHORT).show()
+        }
+
+        color_text_enable_button.setOnClickListener {
+            ok_button.isEnabled = !ok_button.isEnabled
+            ng_button.isEnabled = !ng_button.isEnabled
+            ok_image_button.isEnabled = !ok_image_button.isEnabled
+            ng_image_button.isEnabled = !ng_image_button.isEnabled
+        }
     }
 }
